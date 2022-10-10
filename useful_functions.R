612500892ec22e37b77b64b0bf4ab50547f5a81c
timbender-ncceh <105810134+timbender-ncceh@users.noreply.github.com>
2022-10-10 13:48:35 -0400

    Update useful_functions.R

--- useful_functions.R
+++ useful_functions.R
@@ -1,4 +1,6 @@
 
+# Math-----
+
 # convert decimal numbers to any other baseN number
 dec2baseN <- function(decimal, baseN, separator = "."){
   out <- NULL
@@ -8,3 +10,6 @@ dec2baseN <- function(decimal, baseN, separator = "."){
   out <- paste(c(out, decimal %% baseN),sep = separator,collapse = separator)
   return(out)
 }
+
+
+# Dates----
