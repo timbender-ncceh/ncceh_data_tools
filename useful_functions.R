53e85d79913d17ca133c2b16e31a720f32f1107f
timbender-ncceh <105810134+timbender-ncceh@users.noreply.github.com>
2022-10-10 13:47:59 -0400

    Create useful_functions.R

---
+++ useful_functions.R
@@ -0,0 +1,10 @@
+
+# convert decimal numbers to any other baseN number
+dec2baseN <- function(decimal, baseN, separator = "."){
+  out <- NULL
+  if(decimal > 1){
+    out <- c(out, dec2baseN(as.integer(decimal/baseN),baseN))
+  }
+  out <- paste(c(out, decimal %% baseN),sep = separator,collapse = separator)
+  return(out)
+}
